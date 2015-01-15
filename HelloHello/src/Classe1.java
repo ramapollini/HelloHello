1088866c92909f43eee46269343969687182e323
pollinirama <rama.pollini@gmail.com>
2015-01-15 11:33:31 +0100

    piccoli cambiamenti

--- HelloHello/src/Classe1.java
+++ HelloHello/src/Classe1.java
@@ -2,6 +2,6 @@
 public class Classe1 {
 
 	 public static void main(String[] args) {
-	        System.out.println("Ciao Mondoooooooxxoooooooooo");
+	        System.out.println("Ciao Mondoooooooxxooooooo");
 	    }
 }
