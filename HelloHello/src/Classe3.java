f5fac1dc438996cb4ae9cfba1f21289f81de6eff
ramapollini <rama.pollini@gmail.com>
2015-01-15 14:42:50 +0100

    ok

--- HelloHello/src/Classe3.java
+++ HelloHello/src/Classe3.java
@@ -3,12 +3,8 @@ public class Classe3 {
 
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Ciao io mi chiamo Pinco e anche io faccio una modifica");
-=======
-        System.out.println("Ciao io mi chiamo Remo e faccio una modifica");
->>>>>>> branch 'master' of https://github.com/ramapollini/HelloHello.git
-    }
 
+        System.out.println("Ciao io mi chiamo Remo e faccio una modifica");
+
+	}
 }
-System.out.println("Ciao io mi chiamo Remo e faccio una modifica");
