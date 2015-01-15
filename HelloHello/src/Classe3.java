f53867e04c3dda2ee7c79eb2a7e1b1a878e39036
ramapollini <rama.pollini@gmail.com>
2015-01-15 16:14:26 +0100

    Rama Scrive

--- HelloHello/src/Classe3.java
+++ HelloHello/src/Classe3.java
@@ -3,10 +3,9 @@ public class Classe3 {
 
 
 	public static void main(String[] args) {
-
         System.out.println("Ciao io mi chiamo Remo e faccio una modifica");
+    }
 
-	}
 }
 
-//Amico aggiunge questo commento
+//Anche Rama Scrive
